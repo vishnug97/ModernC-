38a40a55af1b26ffa05a64b87a2ae608512f18bc
vishnug97 <vishnug97@gmail.com>
2025-01-07 08:18:20 +0530

    unique or not

---
+++ string_unique.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	std::string s= "hhhh";/*
+	std::all_of(&s[0],&s[0]+s.length(),[s](char i){return i == s[0];})?std::cout<<"unique":std::cout<<"not unique";
+}
